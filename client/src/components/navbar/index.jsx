f999b1b0a156593820518b942071fbb10b398e5b
Eric Nguyen <11a1eric3@gmail.com>
2022-04-13 00:04:55 +0700

    add panel, slider for homepage

--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
-import locosrc from "../../assets/img/netflix_official_logo_icon.png";
-import { Search, Notifications } from "@material-ui/icons";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import avatarsrc from "../../assets/img/avatar.png";
+import netflixsrc from "../../assets/img/netflix.png";
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    };
     return (
-        <div className="navbar">
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
             <div className="container">
                 <div className="left">
-                    <img src={locosrc} alt="logo" className="img" />
+                    <img src={netflixsrc} alt="Logo" />
                     <span>Homepage</span>
                     <span>Series</span>
                     <span>Movies</span>
@@ -16,9 +23,17 @@ const Navbar = () => {
                     <span>My List</span>
                 </div>
                 <div className="right">
-                    <Search />
+                    <Search className="icon" />
                     <span>KID</span>
-                    <Notifications />
+                    <Notifications className="icon" />
+                    <img src={avatarsrc} alt="Avatar" />
+                    <div className="profile">
+                        <ArrowDropDown className="icon" />
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Logout</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
