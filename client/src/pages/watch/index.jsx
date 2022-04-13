b3daeab272759f686044f10441b4163a75d38d58
Eric Nguyen <11a1eric3@gmail.com>
2022-04-14 00:16:27 +0700

    layout /login, /register, /watch

--- client/src/pages/watch/index.jsx
+++ client/src/pages/watch/index.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import { ArrowBackOutlined } from "@material-ui/icons";
 import "./style.scss";
 
-const Watch = () => {
-    return <div>Watch</div>;
-};
-
-export default Watch;
+export default function Watch() {
+    return (
+        <div className="watch">
+            <div className="back">
+                <ArrowBackOutlined />
+                Home
+            </div>
+            <video
+                className="video"
+                autoPlay
+                progress
+                controls
+                src="https://www.youtube.com/watch?v=FzWG8jiw4XM"
+            />
+        </div>
+    );
+}
